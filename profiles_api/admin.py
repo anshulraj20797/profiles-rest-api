5e0ceddaef2ac01d4e7d6143fb7c6caca36f94ca
anshulraj20797 <anshulraj20797@gmail.com>
2021-06-05 11:46:40 +0530

    API Feed is completed

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 # from profiles_api import models
-from .models import UserProfile
+from .models import ProfileFeedItem, UserProfile
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(ProfileFeedItem)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import base_user
 from django.urls import path, include
 from rest_framework import routers, viewsets
 from profiles_api import views
@@ -6,8 +7,10 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile',views.UserProfileViewSet, base_name='profile')
+router.register('feed',views.UserProfileFeedViewSet, base_name='feed')
 
 urlpatterns = [
     path('hello-view/',views.HelloAPIView.as_view(),name='hello-view'),
+    path('login/',views.UserLoginApiView.as_view(),name='login'),
     path('',include(router.urls)),
 ]
